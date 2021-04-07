a557c9f283491cc0a6e29e6315c20b5f39f5ada1
C22Joshua.Krutz <c22joshua.krutz@usafa.edu>
2021-04-06 22:27:08 -0600

    updates

--- drone_mission.py
+++ drone_mission.py
@@ -393,7 +393,6 @@ def determine_drone_actions(target_point, frame, target_sightings):
                     #   hint: ast_obj_lat, last_obj_lon, drone.airspeed, ast_obj_alt+5, last_obj_heading
                     #   1. move to point here
                     #   2. perform yaw to face in right direction here.
-                    drone_lib.goto_point()
                     drone_lib.goto_point(drone, last_obj_lat, last_obj_lon, drone.airspeed, last_obj_alt + 5, log=log)
                     drone_lib.condition_yaw(drone, last_obj_heading, log=log)
 
